89ad8c00cbc0ae7a13002569ab43589755fb8b0f
hyucknaeKwon <nhk321@naver.com>
2018-05-29 19:10:56 +0900

    package distribute

--- setup.py
+++ setup.py
@@ -1,23 +1,17 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='PD_FC',
-    version='1.0',
+    name='Public_Data_FormatChange',
+    version='1.1.0',
     description='Data Format Change',
     author='Kwon Hyeoknae & Park Duri',
-    author_email='nhk321@naver.com  & twooo.park@gmail.com',
+    author_email='kwonhucnae@gmail.com',
     url='https://github.com/kwonhyucknae/Public_Data_FormatChange',
     download_url='https://github.com/kwonhyucknae/Public_Data_FormatChange/archive/1.0.tar.gz',
     install_requires=[],
     packages=find_packages(exclude = ['docs','tests*']),
     keywords=['Public_Data','Format_Change'],
     python_requires='>=3',
-    package_data={
-        ''
-    },
-    zip_safe=False,
-    classifiers=[
-        'Programming Language :: Python :: 3.6.5'
-    ]
-    
+    zip_safe=False
+
 )
