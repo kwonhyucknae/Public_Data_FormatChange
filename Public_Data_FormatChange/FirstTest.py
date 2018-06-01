8b39831195760da1ede6018bc243c6ae0408c66d
hyucknaeKwon <nhk321@naver.com>
2018-06-01 14:09:48 +0900

    CSV 1 row read

--- Public_Data_FormatChange/FirstTest.py
+++ Public_Data_FormatChange/FirstTest.py
@@ -1,6 +1,10 @@
 #Commit test
 #retest
 #test
+import csv
+from collections import defaultdict
+
+
 def Test(a,b):
     return a+b
 
@@ -26,7 +30,47 @@ def readCSV(loc):
             print(d)
     f.close()
 
+'''
+def writeCSV(loc):
+    f=open(loc,'w',encoding='utf-8',newline='')
+    wr=csv.writer(f)
+    wr.writerow([1,'mkblog'])
+    f.close()
+'''
+
+
+
+
+
+
+#writeCSV("D:\gitpj\Public_Data_FormatChange\Public_Data_FormatChange\\2013년_전년대비_시간대별_교통사고_사망자_현황.csv")
+
+#readCSV("D:\gitpj\Public_Data_FormatChange\Public_Data_FormatChange\\2013년_전년대비_시간대별_교통사고_사망자_현황.csv")
+
+'''
 import sys
 path = sys.argv[0].rsplit("\\",1)
-readCSV(path[0]+"/2013년_전년대비_시간대별_교통사고_사망자_현황.csv")
+print(path[0])
 
+readCSV(path[0]+"/2013년_전년대비_시간대별_교통사고_사망자_현황.csv")
+'''
+
+columns=defaultdict(list)
+
+with open("D:\gitpj\Public_Data_FormatChange\Public_Data_FormatChange\\2013년_전년대비_시간대별_교통사고_사망자_현황.csv",encoding='utf-8') as f:
+    reader=csv.DictReader(f)
+    for row in reader:
+        for(k,v) in row.items():
+            columns[k].append(v)
+
+
+
+print(columns['구분'])
+
+
+
+
+'''
+csv 특정 열 읽어오기
+
+'''
