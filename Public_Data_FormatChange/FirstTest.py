8d2ab16a127a3145bd7c5154e82b587e65bf63e2
twooopark <twooo.park@gmail.com>
2018-06-04 13:41:26 +0900

    Modularize readCSV, update the parameter

--- Public_Data_FormatChange/FirstTest.py
+++ Public_Data_FormatChange/FirstTest.py
@@ -11,25 +11,6 @@ def Test(a,b):
 # loc =="2013년_전년대비_시간대별_교통사고_사망자_현황.csv"
 # 문제점 1. encodig 형식이 맞지 않을 수 있다.
 
-def readCSV(loc):
-    f = open(loc, 'r', encoding="utf-8")
-    datas = [] # 리스트 생성
-    line = f.readline()
-    if not line:
-        exit(0)
-    line = line.strip().replace(" ", "") # 양 끝의 공백, 중간의 공백 제거
-    keys = line.split(",") # 각 줄을 컴마로 구분하여 자름
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        line = line.strip().replace(" ", "")
-        values = line.split(",")
-        for key in keys:
-            d = dict(zip(keys, values)) # 키와 값을 별도로 선언 후 합침
-            print(d)
-    f.close()
-
 '''
 def writeCSV(loc):
     f=open(loc,'w',encoding='utf-8',newline='')
---
+++ Public_Data_FormatChange/readCSV.py
@@ -0,0 +1,35 @@
+import sys
+import csv
+from collections import defaultdict
+
+# init
+datas = defaultdict(list)
+slash = "/"
+
+# get path
+path = sys.argv[0].rsplit(slash,2) 
+
+# enter the filename and encoding 
+encoding = 'euc-kr'
+fileName = "AL_11_D005_20180505.csv"
+filePath = path[0]+slash+fileName
+
+def readCSV(loc, encoding):
+    try :
+        # with ~ as (auto file close)
+        with open(loc, encoding=encoding) as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                for(k,v) in row.items():
+                    datas[k].append(v)
+            print(datas)
+    except UnicodeDecodeError as e:
+        # 어떻게 해야, 자동으로 인코딩 방식을 변경해 줄 수 있을까?
+        print("인코딩 방식 "+encoding+"이(가) 맞지 않습니다.")
+    except FileNotFoundError:
+        # slash = "\\"
+        # filePath = path[0]+slash+fileName
+        print("파일 "+filePath+"이(가) 없습니다.")
+
+# readCSV(filePath, encoding)
+
